0c34e7558d69c5931feb70fd57f250fb476e351e
Michael Zaccardi <mpzaccardi@gmail.com>
2023-03-02 21:05:17 -0500

    Initial Commit

---
+++ SolarGram/SolarGramApp.swift
@@ -0,0 +1,17 @@
+//
+//  SolarGramApp.swift
+//  SolarGram
+//
+//  Created by Lori Merone on 3/2/23.
+//
+
+import SwiftUI
+
+@main
+struct SolarGramApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
